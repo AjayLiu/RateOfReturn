9879c4e34d68460a1870e734741165b340d3f59d
AjayLiu <ajayliu2928@gmail.com>
2020-09-17 20:56:44 -0700

    working formula

--- index.js
+++ index.js
@@ -1,50 +1,17 @@
+//Calculate formula on input
+$("#numerator, #denominator").on("input", function (e) {
+  var num = $("#numerator").val();
+  var den = $("#denominator").val();
+  if (!isNaN(num) && !isNaN(den) && num != "" && den != "") {
+    $("#answer").html(((num / den) * 100).toFixed(2) + "%");
+  } else {
+    $("#answer").html("??");
+  }
+});
+
+//Scroll into view on button click
 $("#viewButton").click(function () {
   document
     .getElementById("aboutSection")
     .scrollIntoView({ behavior: "smooth" });
-});
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
-
-// NAVBAR SCROLLING
-
-var sectionNames = ["about", "signup", "classes", "contact", "gallery", "faq"];
-
-for (let i = 0; i < sectionNames.length; i++) {
-  var name = sectionNames[i];
-  var links = document.getElementsByClassName("link_" + name);
-  for (let j = 0; j < links.length; j++) {
-    var elem = links[j];
-    elem.style.cursor = "pointer";
-    $(links[j]).click(function () {
-      var offset = $("#" + sectionNames[i] + "Section").offset();
-      $("html, body").animate(
-        {
-          scrollTop: offset.top,
-        },
-        1000
-      );
-    });
-  }
-}
-
-//GALLERY
-$("#gallery").slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  dots: true,
 });
